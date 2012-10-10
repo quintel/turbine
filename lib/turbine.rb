1c08c6e58075a0c7017253c6217be5b6c5629807
Dennis Schoenmakers <dennis.schoenmakers@quintel.com>
2012-10-10 12:07:15 +0200

    using require_relative in order to use Pry/Irb

--- lib/turbine.rb
+++ lib/turbine.rb
@@ -1,8 +1,8 @@
 require 'set'
 
 # On with the library...
-require 'turbine/edge'
-require 'turbine/errors'
-require 'turbine/graph'
-require 'turbine/version'
-require 'turbine/vertex'
+require_relative 'turbine/edge'
+require_relative 'turbine/errors'
+require_relative 'turbine/graph'
+require_relative 'turbine/version'
+require_relative 'turbine/vertex'
