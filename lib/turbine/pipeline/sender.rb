f33630c13a52df6180b35bd534394ff8e4c4bb56
Anthony Williams <hi@antw.me>
2012-10-19 20:49:21 +0100

    Add Pipeline::Sender.
    Sender is a Segment which transforms its input by sending a
    +message+ to each input and returning the result.

---
+++ lib/turbine/pipeline/sender.rb
@@ -0,0 +1,62 @@
+module Turbine
+  module Pipeline
+    # A segment which transforms its input by sending a +message+ to each
+    # input and returning the result.
+    #
+    #   ( Pump.new([1, 2]) | Sender.new(:to_s) ).to_a
+    #   # => ['1', '2']
+    #
+    # Each item coming from the source segment must have a public method with
+    # the same name as the +message+.
+    #
+    # Some methods in Turbine return an Array, Collection, or Enumerator as a
+    # sort of "result set" -- such as Node#in, Node#descendants, etc. In these
+    # cases, each element in the result set is yielded separately before
+    # continuing with the next input.
+    #
+    #   pump     = Pump.new(['abc def', 'hij klm'])
+    #   split    = Sender.new(:split, ' ')
+    #
+    #   pipeline = pump | split
+    #
+    #   pipeline.next # => 'abc'
+    #   pipeline.next # => 'def'
+    #   pipeline.next # => 'hij'
+    #   pipeline.next # => 'klm'
+    class Sender < Segment
+
+      # Public: Creates a new Sender segment.
+      #
+      # message - The message (method name) to be sent to each value in the
+      #           pipeline.
+      # args    - Optional arguments to be sent with the message.
+      #
+      # Returns a Sender.
+      def initialize(message, *args)
+        @message = message
+        @args    = args
+        @buffer  = nil
+
+        super()
+      end
+
+      #######
+      private
+      #######
+
+      def process
+        while value = input.public_send(@message, *@args)
+          case value
+          when Array, Enumerator, Collection
+            # Recurse into arrays as the input may return multiple results (as
+            # is commonly the case when calling Node#in, Node#ancestors, etc).
+            value.each { |buffered_value| handle_value(buffered_value) }
+          else
+            handle_value(value)
+          end
+        end
+      end
+
+    end # Sender
+  end # Pipeline
+end # Turbine
---
+++ spec/turbine/pipeline/sender_spec.rb
@@ -0,0 +1,112 @@
+require 'spec_helper'
+
+module Turbine::Pipeline
+  describe Sender do
+    Target = Struct.new(:score)
+
+    context 'when the source item responds to the message' do
+      let(:pipeline) do
+        pump = Pump.new([ Target.new(10), Target.new(42), Target.new(1337) ])
+        send = Sender.new(:score)
+
+        pump | send
+      end
+
+      it 'returns the result of the message' do
+        expect(pipeline.next).to eql(10)
+        expect(pipeline.next).to eql(42)
+        expect(pipeline.next).to eql(1337)
+      end
+    end # when the source item responds to the message
+
+    context 'when the source item returns several results' do
+      let(:source)   { ['a bc', 'def hij klm', 'nop', 'q'] }
+      let(:pipeline) { Pump.new(source) | Sender.new(:split, ' ') }
+
+      it 'yields each result in turn' do
+        expect(pipeline.to_a[0..1]).to eql(%w( a bc ))
+      end
+
+      it 'later continues with the upstream items' do
+        expect(pipeline.to_a[2..-1]).to eql(%w( def hij klm nop q))
+      end
+
+      context 'and the source has an empty array' do
+        let(:source) { ['a bc', '', 'def'] }
+
+        it 'skips to the next present value' do
+          expect(pipeline.to_a).to eql(%w( a bc def ))
+        end
+      end
+    end # when the source item returns several results
+
+    context 'when the source item returns nested arrays' do
+      let(:source)   { ['abc', [['d'], ['e']], 'fgh', []] }
+      let(:pipeline) { Pump.new(source) | Sender.new(:length) }
+
+      it 'does not recuse into nested arrays' do
+        expect(pipeline.to_a).to eql([3, 2, 3, 0])
+      end
+    end # when the source item returns nested arrays
+
+    context 'when the source item returns a Collection' do
+      let(:source)   { [
+        Target.new(Turbine::Collection.new([9])),
+        Target.new(Turbine::Collection.new([8, 7])),
+        Target.new(Turbine::Collection.new([6, 5]))
+      ] }
+
+      let(:pipeline) { Pump.new(source) | Sender.new(:score) }
+
+      it 'expands each collection' do
+        expect(pipeline.to_a).to eql([9, 8, 7, 6, 5])
+      end
+    end
+
+    context 'when the source item is an enumerator' do
+      let(:source) do
+        [ Target.new([1, 2].to_enum), Target.new([3, 4].to_enum) ]
+      end
+
+      let(:pipeline) { Pump.new(source) | Sender.new(:score) }
+
+      it 'expands the enumerators' do
+        expect(pipeline.to_a).to eql([1, 2, 3, 4])
+      end
+    end
+
+    context 'chaining multiple Senders' do
+      let(:pipeline) do
+        Pump.new([1, 2, 3]) | Sender.new(:*, 10) | Sender.new(:to_s)
+      end
+    end
+
+    context 'when the source item raises an error' do
+      let(:pipeline) { (Pump.new([->{ raise 'Oops!' }]) | Sender.new(:call)) }
+
+      it 'propagates the error' do
+        expect { pipeline.next }.to raise_error(/Oops!/)
+      end
+    end # when the source item raises an error
+
+    context 'when the message is to a private method' do
+      HasPrivate = Class.new do
+        private ; def nope ; end
+      end
+
+      let(:pipeline) { Pump.new([HasPrivate.new]) | Sender.new(:nope) }
+
+      it 'raises an error' do
+        expect { pipeline.next }.to raise_error(/private method/)
+      end
+    end # when the message is to a private method
+
+    context 'when the message is to a non-existent method' do
+      let(:pipeline) { Pump.new(['']) | Sender.new(:__invalid__) }
+
+      it 'raises an error' do
+        expect { pipeline.next }.to raise_error(/undefined method/)
+      end
+    end # when the message is to a non-existent method
+  end # Sender
+end # Turbine::Pipeline
