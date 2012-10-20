f4639cfd76b3526903b96b11c71bb8b6addf174a
Anthony Williams <hi@antw.me>
2012-10-20 16:49:08 +0100

    Spec Pipeline::Segment#take.

--- spec/turbine/pipeline/integration_spec.rb
+++ spec/turbine/pipeline/integration_spec.rb
@@ -76,5 +76,31 @@ module Turbine::Pipeline
         pipeline.to_a
       end
     end # Laziness
+
+    describe '#take' do
+      let(:pump)     { Pump.new((1..10).to_a) }
+      let(:filter)   { Filter.new { |*| true } }
+      let(:pipeline) { pump | filter }
+
+      it 'returns the requested number of items' do
+        expect(pipeline.take(4)).to eql([1, 2, 3, 4])
+      end
+
+      it 'returns the full collection when requesting too many items' do
+        expect(pipeline.take(20)).to eql((1..10).to_a)
+      end
+
+      it 'executes lazily' do
+        filter.should_receive(:next).
+          exactly(3).times.and_return { pump.next }
+
+        pipeline.take(3)
+      end
+
+      it 'rewinds on each call' do
+        pipeline.take(2)
+        expect(pipeline.take(3)).to eql([1, 2, 3])
+      end
+    end # #take
   end # Pipeline
 end # Turbine::Pipeline
